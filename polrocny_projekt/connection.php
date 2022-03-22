36825e5295e08d85c84bcf3e98c1aa48da729d8c
SamuelKocifaj <75687509+SamuelKocifaj@users.noreply.github.com>
2022-03-22 23:36:40 +0100

    Add files via upload

--- polrocny_projekt/connection.php
+++ polrocny_projekt/connection.php
@@ -6,16 +6,8 @@ $database="stranka";
 
 $conn=new mysqli($servername,$username,$password,$database);
 
-$user_id=$_POST["User_ID"];
-$email_id=$_POST["Email_ID"];
-$pass=$_POST["Password"];
-$s="select *from usertable where Email_ID="$email_id"";
-$result=mysqli_query($con,$s);
-$num= mysqli_num_rows($result);
-if($num==1){
-    echo"email already taken";
-}else{
-    $reg="insert into usertable(User_ID,Email_ID,Password )values ("$user_id","$email_id","$pass")";
-    mysqli_query($con,$reg);
-    echo"Registration complete";
+if($conn -> connect_error){
+    die("Spojenie sa nepodarilo".connect_error);
 }
+// echo "spojenie sa podarilo";
+?>
